57cc01fbcdd222ef6410a4b5da38fa29c0eaa932
oldshiji <1655664358@qq.com>
2018-12-31 13:45:23 +0800

    edit start file

--- bin/start.php
+++ bin/start.php
@@ -6,11 +6,10 @@
  * Time: 0:44
  */
 define('SIWO_PATH',realpath(getcwd()));
-$autoLoadFile = SIWO_PATH.'vendor/autoload.php';
+$autoLoadFile = SIWO_PATH.'/vendor/autoload.php';
 if (file_exists($autoLoadFile)){
     require $autoLoadFile;
 }else{
-    echo $autoLoadFile."\n";
     die("require composer autoload fail\n");
 }
 (new \Siwo\Foundation\Application())->run();
