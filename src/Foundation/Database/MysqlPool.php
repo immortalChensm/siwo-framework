4eaa389e6ec78a67111722e2492756ba4981be19
oldshiji <1655664358@qq.com>
2019-01-01 22:40:25 +0800

    mysql connection pool

--- src/Foundation/Database/MysqlPool.php
+++ src/Foundation/Database/MysqlPool.php
@@ -10,7 +10,7 @@ use Siwo\Foundation\Traits\Singleton;
 
 class MysqlPool
 {
-    protected $instance;
+    protected static $instance;
 
     use Singleton;
     /**
@@ -22,7 +22,7 @@ class MysqlPool
      * RedisPool constructor.
      * @param int $size 连接池的尺寸
      */
-    function __construct($size = 100)
+    function __construct($size = 10)
     {
         $this->pool = new Swoole\Coroutine\Channel($size);
         for ($i = 0; $i < $size; $i++)
